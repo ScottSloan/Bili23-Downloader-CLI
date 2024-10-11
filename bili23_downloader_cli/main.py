12b082b1ce1e19f7be908b319505c0955ab8fa36
SevenDreamer <945186289@qq.com>
2024-10-11 21:39:45 +0800

    🧹 chore: 大致定义可执行的命令

--- bili23_downloader_cli/main.py
+++ bili23_downloader_cli/main.py
@@ -1,48 +1,118 @@
-from typing import Annotated, List, Optional
-from typer import Exit, Typer, Argument, Option
+from enum import Enum
+from typing import Annotated, Any, Dict
 
-from bili23_downloader_cli.cli.common import check_ffmpeg_available
+from typer import Argument, Option, Typer
 
 app = Typer(
     no_args_is_help=True,
     rich_markup_mode="rich",
     help="Bilibili视频下载器",
-    epilog="Made with :heart:  in [blue]Seven Dreamer[/blue]"
+    epilog="Made with :heart:  in [blue]Seven Dreamer[/blue]",
 )
 
-@app.command("version")
-def show_version():
-    """Show version"""
-    version = "2.0.1"
-    print(f"bili23 Downloader CLI Version: {version}")
+
+class VideoType(Enum):
+    Bangumi = "番剧"
+    """番剧"""
+    Movie = "电影"
+    """电影"""
+    Guochuang = "国创"
+    """国创"""
+    Documentary = "纪录片"
+    """纪录片"""
+    Normal = "用户投稿视频"
+    """用户投稿视频"""
 
 
+def get_video_type(video_type: int) -> VideoType:
+    """
+    通过视频信息判断视频的是什么类型
+    """
+    match video_type:
+        case 1:
+            return VideoType.Bangumi
+        case 2:
+            return VideoType.Movie
+        case 3:
+            return VideoType.Guochuang
+        case 4:
+            return VideoType.Guochuang
+        case _:
+            return VideoType.Normal
 
-# @app.command("info", help="Get video info")
-# def get_url_info(url: str):
-#     """获取视频信息"""
-#     print(f"this is url {url} info:xxxxx")
 
-@app.command(help="Download video",no_args_is_help=True)
+def get_version() -> str:
+    """从pyproject.toml中获取版本"""
+    raise NotImplementedError
+
+
+def show_version(enabled: bool):
+    """显示版本信息"""
+    if enabled:
+        version = get_version()
+        print(f"Bili23 Version: {version}")
+
+
+def get_video_info(url: str) -> Dict[str, Any]:
+    """
+    获取视频信息
+    """
+    # TODO: 这里发送请求过去，大概就是写一个 request.get()  什么的方法过去就是了
+
+    raise NotImplementedError
+
+
+@app.command()
 def download(
-    url: Annotated[List[str], Argument(help="视频链接",show_default=False)]
+    url: Annotated[
+        str,
+        Argument(help="视频链接,暂不支持输入多个url地址", show_default=False),
+    ],
+    show_info: Annotated[
+        bool, Option("--info", "-i")
+    ] = False,  # 下载的时候默认不显示视频的信息，如果需要显示，则跟 --info / -i
 ):
-    """下载视频"""
-    print(f"downloading ... {url}")
+    """下载"""
+
+    video_info = get_video_info(url)
+    get_video_type(video_info["type"])
+
+    if show_info:
+        # TODO： 需要接rich 打印视频的信息
+        ...
+
+    # TODO: 这里可以跟上promt 进行交互
+    # 是否选择 视频品质，默认为配置的value
+    # 是否选择 音频品质, 同上
+    # ...
+
+
+@app.command()
+def repo():
+    """
+    本地仓库列表
+
+    用来查看已经下载的视频，所在的位置，查看视频的详情信息等，
+    """
+    raise NotImplementedError
+
 
 @app.command()
 def config():
-    """
-    cli 配置
-    """
+    """查看 or 编辑 配置"""
+    raise NotImplementedError
 
+
+# TODO： 需要支持输入多个url 进行下载
 @app.callback()
 def main(
-    # version: Annotated[bool, Option(callback=show_version, is_eager=True)] = False,
+    version: Annotated[
+        bool,
+        Option(
+            "--version", "-v", callback=show_version, help="show version", is_eager=True
+        ),
+    ] = False,
 ):
-    """下载bilibili的视频"""
-    check_ffmpeg_available()
-
-
-if __name__ == "__main__":
-    app()
+    """
+    BiliBili 视频下载工具
+    """
