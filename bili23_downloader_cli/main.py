a98aca11510beb411632be288b3bd28e5ed08a7a
sevenbee <945186289@qq.com>
2024-10-11 18:17:37 +0800

    🧹 chore: add all files

--- bili23_downloader_cli/main.py
+++ bili23_downloader_cli/main.py
@@ -1,25 +1,47 @@
-import typer
+from typing import Annotated, List, Optional
+from typer import Exit, Typer, Argument, Option
 
-app = typer.Typer()
+from bili23_downloader_cli.cli.common import check_ffmpeg_available
 
+app = Typer(
+    no_args_is_help=True,
+    rich_markup_mode="rich",
+    help="Bilibili视频下载器",
+    epilog="Made with :heart:  in [blue]Seven Dreamer[/blue]"
+)
+
+@app.command("version")
+def show_version():
+    """Show version"""
+    version = "2.0.1"
+    print(f"bili23 Downloader CLI Version: {version}")
+
+
+
+# @app.command("info", help="Get video info")
+# def get_url_info(url: str):
+#     """获取视频信息"""
+#     print(f"this is url {url} info:xxxxx")
+
+@app.command(help="Download video",no_args_is_help=True)
+def download(
+    url: Annotated[List[str], Argument(help="视频链接",show_default=False)]
+):
+    """下载视频"""
+    print(f"downloading ... {url}")
 
 @app.command()
-def hello(name: str):
-    print(f"hello {name}")
+def config():
+    """
+    cli 配置
+    """
 
-
-@app.command()
-def goodbye(name: str):
-    print(f"bye~ {name}")
-
-
-def main():
-    app()
-
-
-@app.command(name="info", help="获取视频信息")
-def get_url_info(url: str):
-    print(f"this is url {url} info:xxxxx")
+@app.callback()
+def main(
+    # version: Annotated[bool, Option(callback=show_version, is_eager=True)] = False,
+):
+    """下载bilibili的视频"""
+    check_ffmpeg_available()
 
 
 if __name__ == "__main__":
