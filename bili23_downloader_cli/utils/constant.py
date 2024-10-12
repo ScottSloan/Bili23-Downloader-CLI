668405f6543cf7d9b901fb935dd72176e8930754
sevenbee <945186289@qq.com>
2024-10-12 12:05:54 +0800

    ✨ feat(constant): 将QualityType 和 CodecType 继承自StrEnum

--- bili23_downloader_cli/utils/constant.py
+++ bili23_downloader_cli/utils/constant.py
@@ -3,7 +3,43 @@
 """
 
 from enum import Enum
+from sys import version_info
 
+# # 自定义字符串枚举
+if version_info < (3,11):
+    class StrEnum(str, Enum):
+        """
+        Enum where members are also (and must be) strings
+        """
+
+        def __new__(cls, *values):
+            "values must already be of type `str`"
+            if len(values) > 3:
+                raise TypeError('too many arguments for str(): %r' % (values, ))
+            if len(values) == 1:
+                # it must be a string
+                if not isinstance(values[0], str):
+                    raise TypeError('%r is not a string' % (values[0], ))
+            if len(values) >= 2:
+                # check that encoding argument is a string
+                if not isinstance(values[1], str):
+                    raise TypeError('encoding must be a string, not %r' % (values[1], ))
+            if len(values) == 3:
+                # check that errors argument is a string
+                if not isinstance(values[2], str):
+                    raise TypeError('errors must be a string, not %r' % (values[2]))
+            value = str(*values)
+            member = str.__new__(cls, value)
+            member._value_ = value
+            return member
+
+        def _generate_next_value_(name, start, count, last_values):
+            """
+            Return the lower-cased version of the member name.
+            """
+            return name.lower()
+else:
+    from enum import StrEnum
 
 quality_map = {
     "超高清 8K": 127,
@@ -20,7 +56,7 @@ quality_map = {
 codec_map = {"AVC/H.264": "avc", "HEVC/H.265": "hevc", "AV1": "av1"}
 
 
-class VideoQuality(Enum):
+class VideoQuality(StrEnum):
     """视频品质"""
 
     UHD_8K = "超高清 8K"  # 127
@@ -45,7 +81,7 @@ class VideoQuality(Enum):
     """流畅 360P"""
 
 
-class VideoCodec(Enum):
+class VideoCodec(StrEnum):
     """视频编解码"""
 
     AVC = "AVC/H.264"  # "avc"
